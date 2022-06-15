0321b130bc6edeea91a83ac0fdecdd2cb27f4bdb
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-06-15 21:35:05 +0900

    withContextを使ってスレッドの切り替え

--- app/src/main/java/com/example/coroutine_sample2_m1/MainActivity.kt
+++ app/src/main/java/com/example/coroutine_sample2_m1/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.coroutine_sample2_m1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val scope = CoroutineScope(Job() + Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scope.launch {
+            Log.d("Tatsuya🐲", "fetch前: ${this.coroutineContext}")
+            val result = fetch()
+            Log.d("Tatsuya🐲", "fetch後: ${this.coroutineContext}")
+            Log.d("Tatsuya🐲", "onCreate: ${result}")
+        }
+    }
+
+    private suspend fun fetch(): String {
+        return withContext(Dispatchers.IO) {
+            Log.d("Tatsuya🐲", "fetch内部: ${this.coroutineContext}")
+            delay(1000L)
+            "⚽️"
+        }
     }
 }
